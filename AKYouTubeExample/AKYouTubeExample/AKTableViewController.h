6e7f8b2351f3f62d403853c4499773f088c4b716
Anton Pomozov <pomozov@shtrih-m.ru>
2013-10-08 13:46:24 +0400

    [Add]: New example with navigation controller.

---
+++ AKYouTubeExample/AKYouTubeExample/AKTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  AKTableViewController.h
+//  AKYouTubeExample
+//
+//  Created by Anton Pomozov on 08.10.13.
+//  Copyright (c) 2013 Akademon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "YTConnector.h"
+
+@interface AKTableViewController : UITableViewController <YTConnectorDelegate>
+
+@end
---
+++ AKYouTubeExample/AKYouTubeExample/AKTableViewController.m
@@ -0,0 +1,64 @@
+//
+//  AKTableViewController.m
+//  AKYouTubeExample
+//
+//  Created by Anton Pomozov on 08.10.13.
+//  Copyright (c) 2013 Akademon Ltd. All rights reserved.
+//
+
+#import "AKTableViewController.h"
+#import "AKConstants.h"
+
+@interface AKTableViewController ()
+
+@end
+
+@implementation AKTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+@end
