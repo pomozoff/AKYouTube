5dac12af3c0cb4daa1aa3a53a7cb228615549095
Anton Pomozov <pomozov@shtrih-m.ru>
2013-10-11 18:47:17 +0400

    [Add]: Implementation of custom login view controller.

---
+++ AKYouTubeExample/AKYouTubeExample/AKCustomLoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  AKCustomLoginViewController.h
+//  AKYouTubeExample
+//
+//  Created by Anton Pomozov on 11.10.13.
+//  Copyright (c) 2013 Akademon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "YTConnector.h"
+
+@interface AKCustomLoginViewController : UIViewController <YTLoginViewControllerInterface>
+
+@end
---
+++ AKYouTubeExample/AKYouTubeExample/AKCustomLoginViewController.m
@@ -0,0 +1,33 @@
+//
+//  AKCustomLoginViewController.m
+//  AKYouTubeExample
+//
+//  Created by Anton Pomozov on 11.10.13.
+//  Copyright (c) 2013 Akademon Ltd. All rights reserved.
+//
+
+#import "AKCustomLoginViewController.h"
+
+@interface AKCustomLoginViewController ()
+
+@property (nonatomic, strong) IBOutlet UIWebView *webView;
+
+@end
+
+@implementation AKCustomLoginViewController
+
+#pragma mark - Synthesizes
+
+@synthesize webView = _webView;
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
